7a2aca79bca747dc9402daf2ca94c45de0eb148b
EyaOuenniche <eya.ouenniche@medtech.tn>
2024-02-16 00:06:46 +0100

    added a footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Copyright © {new Date().getFullYear()} SMU Lens</p>
+    </footer>
+  );
+}
+
+export default Footer;
