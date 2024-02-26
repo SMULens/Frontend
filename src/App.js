f1df236865e77dadec120e905be3b21b6d83c95b
EyaOuenniche <eya.ouenniche@medtech.tn>
2024-02-26 16:00:37 +0100

    Updated The Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Routes } from 'react-router-dom';
 
-function App() {
+import Profile from "./pages/Profile"
+import ListOfUsers from "./pages/ListOfUsers"
+import CreateUser from './pages/CreateUser';
+import Nav from './components/Nav';
+
+const App = () => {
+  // Mock user state
+  const user = {
+    role: 'admin', // or 'ACC'
+    name: 'Admin'
+  };
+
   return (
-    <div className="App">
-      
-      <Nav/>
-      <Footer/>
-    </div>
+    <Router>
+      <div>
+        <Nav user={user} />
+        <Routes>
+          <Route path="/profile" component={Profile} />
+          <Route path="/list-of-users" component={ListOfUsers} />
+          <Route path="/create-user" component={CreateUser} />
+        
+          </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,37 @@
+// NavigationBar.js
 import React from 'react';
-import "./Nav.css"
-function Nav() {
+import { Link } from 'react-router-dom';
+import './Nav.css';
+
+const Nav = ({ user }) => {
+  const { role, name } = user;
+
+
   return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <span>Hello, ACC</span>
+   
+      <div className="navbar">
+        <div className="navbar-left">
+          <p className="navbar-brand">Welcome, {name}</p>
+        </div>
+        <div className="navbar-right">
+          <ul className="navbar-nav">
+            {role === 'ACC' && (
+              <>
+                <li className="nav-item"><Link to="/profile" className="nav-link">Profile</Link></li>
+                <li className="nav-item"><Link to="/profile" className="nav-link">Logout</Link></li>
+              </>
+            )}
+            {role === 'admin' && (
+              <>
+                <li className="nav-item"><Link to="/list-of-users" className="nav-link">List of Users</Link></li>
+                <li className="nav-item"><Link to="/create-user" className="nav-link">Create User</Link></li>
+                <li className="nav-item"><Link to="/profile" className="nav-link">Logout</Link></li>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
-      <div className="navbar-right">
-        <button className="navbar-button">Profile</button>
-        <button className="navbar-button">Logout</button>
-      </div>
-    </nav>
-  );
-}
+    );
+};
 
 export default Nav;
